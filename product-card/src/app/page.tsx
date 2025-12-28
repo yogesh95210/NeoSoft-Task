842f4dc1c36d6f998a003b993e655e7dd5da3a6d
Yogesh Sharma <webdeveloper.yogesh63@gmail.com>
2025-12-28 21:27:25 +0530

    removal of redundant code

--- product-card/src/app/page.tsx
+++ product-card/src/app/page.tsx
@@ -1,21 +1,9 @@
-import ProductCard from "@/Component/ProductCard";
-
-
 export default function Home() {
- const product = {
-  image: 'https://images.unsplash.com/photo-1606813902911-0d6e5979cbbf?auto=format&fit=crop&w=800&q=80',
-  title: 'Wireless Bluetooth Headphones',
-  description: 'Experience crystal-clear sound and deep bass with our wireless Bluetooth headphones. Perfect for music, calls, and travel — with up to 20 hours of battery life.',
-};
 
 
   return (
     <main className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
-      <ProductCard
-        image={product.image}
-        title={product.title}
-        description={product.description}
-      />
+      <h2>Sign In</h2>
     </main>
   );
 }
